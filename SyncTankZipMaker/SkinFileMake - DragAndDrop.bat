a60d9fb50a42828cd97e6a10dcf7cb029e836b43
ml37 <56399130+ml37@users.noreply.github.com>
2022-04-24 16:49:33 +0900

    Update SkinFileMake - DragAndDrop.bat

--- SyncTankZipMaker/SkinFileMake - DragAndDrop.bat
+++ SyncTankZipMaker/SkinFileMake - DragAndDrop.bat
@@ -74,7 +74,6 @@ mkdir %FileName%\vehicles\%country%\%dragfilename%\PSD
 xcopy DInput\*.dds %FileName%\vehicles\%country%\%dragfilename% /e
 xcopy DInput\*.psd %FileName%\vehicles\%country%\%dragfilename%\PSD /e
 cd DOutput
-echo.>Installed_by_SyncTank.walalaru walalaru
 tar -cvzf %FileName%.zip %FileName%
 rmdir /s /q %FileName%
 move %FileName%.zip %StartPosition%
