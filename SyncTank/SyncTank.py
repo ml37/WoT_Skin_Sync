32d4bae729af31e15aec4d6044e8b27d0a79a96b
ml37 <richiemu0416@gmail.com>
2022-06-30 14:31:39 +0900

    move SyncTank.py

--- SyncTank/SyncTank.py
+++ SyncTank/SyncTank.py
@@ -5,6 +5,12 @@ from PyQt5 import uic
 import sys
 form_class = uic.loadUiType("SyncTank.ui")[0]
 
+
+f = open('ClientLocation.inf', 'r')
+client_location = f.readline()
+print(client_location)
+print(f)
+#for문으로 readline으로 한줄씩 읽어서 행렬로 저장
 class WindowClass(QMainWindow, form_class) :
     def __init__(self) :
         super().__init__()
