24e3622a286cfb3f1349d2062e444703632244fa
ml37 <richiemu0416@gmail.com>
2022-06-30 14:27:56 +0900

    Update SyncTank.py

--- SyncTank.py
+++ SyncTank.py
@@ -1,3 +1,18 @@
-import PyQt5
-from PyQt5 import QtCore, QtGui, QtWidgets, uic
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5 import uic
+import sys
 form_class = uic.loadUiType("SyncTank.ui")[0]
+
+class WindowClass(QMainWindow, form_class) :
+    def __init__(self) :
+        super().__init__()
+        self.setupUi(self)
+    
+if __name__ == "__main__" :
+    app = QApplication(sys.argv)
+    myWindow = WindowClass()
+    myWindow.show()
+    app.exec_()
+    sys.exit()
