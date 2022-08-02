c52d01199a537f664868ddf5d354572507639d7f
ml37 <56399130+ml37@users.noreply.github.com>
2022-08-03 01:21:17 +0900

    Update SyncTank.py
    -add download from site
    -move zip to temp
    
    ToDo
    -install

--- SyncTank/SyncTank.py
+++ SyncTank/SyncTank.py
@@ -4,46 +4,59 @@ from PyQt5.QtGui import *
 from PyQt5 import uic
 import sys
 import os
+import urllib.request
+import shutil
 form_class = uic.loadUiType("SyncTank.ui")[0]
 
-print(os.listdir())
+#print(os.listdir())
+print('@'*50)
 f = open('SyncTank\ClientLocation.inf', 'r')
 client_location = f.readline()
 print(client_location)
-print(f)
 f.close()
-
-#for문으로 readline으로 한줄씩 읽어서 행렬로 저장
-#read "SyncTank\Skinlist.inf", readlines(), each one line and sort with list
+print('@'*50)
+DLServer = 'http://' + 'mashiro37.i234.me'
+DLSkinlist = DLServer + '/WoTskin/' + 'Skinlist.inf'
+DLtemp = client_location + '\\SkinTemp' + '\\PySyncTank\\ + \\DLtemp\\'
+print(DLSkinlist)
 class WindowClass(QMainWindow, form_class) :
     def __init__(self) :
         super().__init__()
         self.setupUi(self)
         self.load_skin_list()
         self.listWidget.itemClicked.connect(self.on_item_clicked)
+        self.btn_download.clicked.connect(self.download)
     def load_skin_list(self):
         f = open('SyncTank\Skinlist.inf', 'r')
         lines = f.readlines()
         for i in lines[0:]:
             i = i.strip()
             text = i.split('_')
-            print(text[0])
-            print(i)
+            #print(text[0])
+            #print(i)
             self.listWidget.addItem(i)
             self.label.setText('Total ' + str(self.listWidget.count()) + ' Skins')
         print('@'*50)
         f.close()
     def on_item_clicked(self, item):
         text = item.text().split('_')
-        print(text[0])
-        print(len(text))
         wa = ''
         for i in range(1,len(text)):
             wa = wa + text[i] + ' '
         self.number.setText(text[0])
         self.name.setText(wa)
-        print(wa)
-        #text[1:len(text)])
+        global vehicle_number
+        global vehicle_name
+        vehicle_number=text[0]
+        vehicle_name=wa
+    def download(self): 
+        print('!'*50)
+        print(self.listWidget.currentItem().text())
+        downloadURL = DLServer + '/WoTskin/' + 'Skin/' + vehicle_number + '.zip'
+        print(downloadURL)
+        urllib.request.urlretrieve(downloadURL, vehicle_number + '.zip')
+        shutil.move(vehicle_number + '.zip', DLtemp)
+        print('!'*50)
 if __name__ == "__main__" :
     app = QApplication(sys.argv)
     myWindow = WindowClass()
