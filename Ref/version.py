742005e72cad8cd3d2f7b374f92b337b3082c21d
ml37 <56399130+ml37@users.noreply.github.com>
2022-08-03 21:15:56 +0900

    Create version.py

---
+++ Ref/version.py
@@ -0,0 +1,31 @@
+from http import client
+from multiprocessing.connection import wait
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5 import uic
+import sys
+import os
+import urllib.request
+from urllib.request import Request, urlopen
+import shutil
+import zipfile
+import subprocess
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTextEdit, QAction, QFileDialog
+from PyQt5.QtGui import *
+import PyQt5.QtGui
+import re
+
+p = re.compile('.[.]..[.].[.].')
+os.chdir('res_mods')
+for i in os.listdir():
+    d = p.findall(i)
+    if d == []:
+        continue
+    print(d)
+print(d)
+    
+
+
+
+    
