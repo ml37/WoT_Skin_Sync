6bcfea40db06e7752051a0ce2d21974097c79973
ml37 <56399130+ml37@users.noreply.github.com>
2022-08-06 22:38:16 +0900

    Update SyncTank.py

--- SyncTank/SyncTank.py
+++ SyncTank/SyncTank.py
@@ -8,6 +8,7 @@ import urllib.request
 from urllib.request import Request, urlopen
 import shutil
 import zipfile
+import threading
 form_class = uic.loadUiType("SyncTank.ui")[0]
 
 ########################################################
@@ -64,15 +65,7 @@ ZipMaketemp = client_location + '\\SkinTemp' + '\\PySyncTank\\' + '\\ZipMaketemp
 country = {'A':'american', 'GB':'british', 'Ch':'chinese', 'Cz':'czech', 'F':'french', 'G':'german', 'It':'italy', 'J':'japan', 'Pl':'poland', 'R':'russian', 'S':'sweden'}
 countryimg = {'A':'usa', 'GB':'uk', 'Ch':'china', 'Cz':'czech', 'F':'france', 'G':'germany', 'It':'italy', 'J':'japan', 'Pl':'poland', 'R':'ussr', 'S':'sweden'}
 ########################################################
-#Find Version.inf and read game version.
-if os.path.isfile(DataFolder + '\Version.inf') == False:
-    print('c:\SyncTank\Version.inf is not found')
-    try:
-        urllib.request.urlretrieve(DLVersion, DataFolder + '/Version.inf')
-    except urllib.error.HTTPError as e:
-        print(e.__dict__)
-    except urllib.error.URLError as e:
-        print(e.__dict__)
+#Find SkinList.inf. If Not Exists, Download Skinlist.inf.
 if os.path.isfile(DataFolder + '\Skinlist.inf') == False:
     print('c:\SyncTank\Skinlist.inf is not found')
     try:
@@ -81,12 +74,26 @@ if os.path.isfile(DataFolder + '\Skinlist.inf') == False:
         print(e.__dict__)
     except urllib.error.URLError as e:
         print(e.__dict__)
-Version = 'error'
+########################################################
+#Find Version.inf. If not found, create it. Version.inf is used to store the version information.
+if os.path.isfile(DataFolder + '\Version.inf') == False:
+    print('c:\SyncTank\Version.inf is not found')
+    try:
+        urllib.request.urlretrieve(DLVersion, DataFolder + '/Version.inf')
+    except urllib.error.HTTPError as e:
+        print(e.__dict__)
+    except urllib.error.URLError as e:
+        print(e.__dict__)
+########################################################
+#Version.inf is read and stored in version.
+Version = 'error' 
 with open(DataFolder + '\Version.inf', 'r') as f:
     Version = f.readline()
-    print(f'Version : {Version}')
+    print(f'Notice : Version is {Version}')
+########################################################
+#Critical Error
 if client_location == 'error':
-    print('client location is not found')
+    print('client location is not found') #initialte phaze have a set client location. No need to fix
 else:
     if os.path.isdir(client_location + '\SkinTemp') == False:   
         os.mkdir(client_location + '\\SkinTemp')
@@ -109,33 +116,39 @@ class WindowClass(QMainWindow, form_class) :
     def __init__(self) :
         super().__init__()
         self.setupUi(self)
+        self.setWindowTitle('SyncTank')
+        self.setWindowIcon(QIcon('icon.ico'))
+        self.setAcceptDrops(True)
         self.load_skin_list()
+        ########################################################
+        #connect
         self.listWidget.itemClicked.connect(self.on_item_clicked)
         self.btn_download.clicked.connect(self.download)
-        self.btn_download.setEnabled(False)
         self.btn_opencl.clicked.connect(self.opencl)
         self.btn_openus.clicked.connect(self.openus)
         self.btn_manuallist.clicked.connect(self.manuallist)
         self.btn_all.clicked.connect(self.all)
         self.btn_cleartemp.clicked.connect(self.cleartemp)
-        self.btn_openus.setEnabled(False)
-        self.btn_manuallist.setEnabled(False)
         self.btn_reload.clicked.connect(self.load_skin_list)
         self.btn_open_skin.clicked.connect(self.open_skin)
+        self.btn_opentemp.clicked.connect(self.opentemp)
+        ########################################################
+        #setEnabled
         self.btn_open_skin.setEnabled(False)
-        self.setAcceptDrops(True)
+        self.btn_download.setEnabled(False)
+        self.btn_openus.setEnabled(False)
+        self.btn_manuallist.setEnabled(False)
+        ########################################################
         selClientLoc = QAction('Select Game Client Location', self)
         selClientLoc.triggered.connect(self.selClientLocation)
-        self.setWindowIcon(QIcon('icon.png'))
-        
-        selServerLoc = QAction('Select Upload Server Location', self)
+        selServerLoc = QAction('Select Upload Server Location(Option for Server Admin, Not For User!!)', self)
         selServerLoc.triggered.connect(self.selServerLocation)
         menubar = self.menuBar()
         menubar.setNativeMenuBar(False)
-        fileMenu = menubar.addMenu('&Location')
+        fileMenu = menubar.addMenu('&Load')
         fileMenu.addAction(selClientLoc)
         fileMenu.addAction(selServerLoc)
-        self.setWindowTitle('SyncTank')
+        ########################################################
         if client_location == 'error':
             fname = QFileDialog.getExistingDirectory(self, 'Select Game Client(World_of_Tanks_ASIA) Location', './')
             if fname:
@@ -151,10 +164,8 @@ class WindowClass(QMainWindow, form_class) :
     
     def selClientLocation(self):
         fname = QFileDialog.getExistingDirectory(self, 'Select Game Client(World_of_Tanks_ASIA) Location', './')
-
         if fname[0]:
             f = open(fname[0], 'r')
-
             with f:
                 data = f.read()
                 print(data)
@@ -168,9 +179,9 @@ class WindowClass(QMainWindow, form_class) :
     def manuallist(self):
         path = server_location
         file_list = os.listdir(path)
-        file_list_py = [file for file in file_list if file.endswith(".zip")]
+        file_list_zip = [file for file in file_list if file.endswith(".zip")]
         f = open(server_location + '\Skinlist.inf', 'w')
-        for file in file_list_py:
+        for file in file_list_zip:
             file = file.replace('.zip', '')
             f.write(file + '\n')
         f.close()
@@ -182,6 +193,12 @@ class WindowClass(QMainWindow, form_class) :
         if os.path.isdir(client_location + '\\SkinTemp' + '\\PySyncTank' + '\\Unziptemp') == True:
             shutil.rmtree(client_location + '\\SkinTemp' + '\\PySyncTank' + '\\Unziptemp')
             os.mkdir(client_location + '\\SkinTemp\\PySyncTank\\Unziptemp')
+    def opentemp(self):
+        print('opentemp')
+        if os.path.isdir(client_location + '\\SkinTemp' + '\\PySyncTank' + '\\DLtemp') == True:
+            os.startfile(client_location + '\\SkinTemp' + '\\PySyncTank' + '\\DLtemp')
+        if os.path.isdir(client_location + '\\SkinTemp' + '\\PySyncTank' + '\\Unziptemp') == True:
+            os.startfile(client_location + '\\SkinTemp' + '\\PySyncTank' + '\\Unziptemp')
     def load_skin_list(self):
         self.listWidget.clear()
         try:
@@ -209,9 +226,12 @@ class WindowClass(QMainWindow, form_class) :
         self.lbl_clientlocation.setText('Client Location : ' + client_location)
         self.lbl_serverlocation.setText('Server Location : ' + server_location)
         self.lbl_DLserver.setText('DL Server : ' + DLServer)
+
     def on_item_clicked(self, item):
         self.btn_open_skin.setEnabled(True)
         self.btn_download.setEnabled(True)
+        self.lbl_img.setHidden(True)
+        self.lbl_img_2.setHidden(True)
         text = item.text().split('_')
         wa = ''
         for i in range(1,len(text)):
@@ -229,39 +249,48 @@ class WindowClass(QMainWindow, form_class) :
         string = vehicle_number
         vehicle_Countrycode = ''.join([i for i in string if not i.isdigit()])
         vehicle_country = country[vehicle_Countrycode]
-        url = 'http://tanks.gg/img/tanks/' + countryimg[vehicle_Countrycode] + '-' + lawname + '.png'
-        try:
-            image=Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-        except urllib.error.HTTPError as e:
-            print(e.__dict__)
-            self.btn_download.setEnabled(False)
-        except urllib.error.URLError as e:
-            print(e.__dict__)
-            self.btn_download.setEnabled(False)
-            self.lbl_error.setText('Error!')
-            self.lbl_error_2.setText(str(e.reason))
-        image_data = urlopen(image).read()
-        self.qPixmapWebVar = QPixmap()
-        self.qPixmapWebVar.loadFromData(image_data)
-        self.qPixmapWebVar = self.qPixmapWebVar.scaledToWidth(360) # 480x300 320x200 360x240
-        self.lbl_img.setPixmap(self.qPixmapWebVar)
-        url_flag = 'http://tanks.gg/img/nations/germany.svg'
-        url_flag = 'http://tanks.gg/img/nations/' + countryimg[vehicle_Countrycode] + '.svg'
-        try:
-            image_flag = Request(url_flag, headers={'User-Agent': 'Mozilla/5.0'})
-        except urllib.error.HTTPError as e:
-            print(e.__dict__)
-            self.btn_download.setEnabled(False)
-        except urllib.error.URLError as e:
-            print(e.__dict__)
-            self.btn_download.setEnabled(False)
-            self.lbl_error.setText('Error!')
-            self.lbl_error_2.setText(str(e.reason))
-        image_data_flag = urlopen(image_flag).read()
-        self.qPixmapWebVar_flag = QPixmap()
-        self.qPixmapWebVar_flag.loadFromData(image_data_flag)
-        self.qPixmapWebVar_flag = self.qPixmapWebVar_flag.scaledToWidth(100)
-        self.lbl_img_2.setPixmap(self.qPixmapWebVar_flag)
+        def load_img_from_web():
+                url = 'http://tanks.gg/img/tanks/' + countryimg[vehicle_Countrycode] + '-' + lawname + '.png'
+                try:
+                    image=Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+                except urllib.error.HTTPError as e:
+                    print(e.__dict__)
+                    self.btn_download.setEnabled(False)
+                except urllib.error.URLError as e:
+                    print(e.__dict__)
+                    self.btn_download.setEnabled(False)
+                    self.lbl_error.setText('Error!')
+                    self.lbl_error_2.setText(str(e.reason))
+                image_data = urlopen(image).read()
+                self.qPixmapWebVar = QPixmap()
+                self.qPixmapWebVar.loadFromData(image_data)
+                self.qPixmapWebVar = self.qPixmapWebVar.scaledToWidth(360) # 480x300 320x200 360x240
+                self.lbl_img.setPixmap(self.qPixmapWebVar)
+                self.lbl_img.setHidden(False)
+        def load_flag_from_web():
+                url_flag = 'http://tanks.gg/img/nations/' + countryimg[vehicle_Countrycode] + '.svg'
+                try:
+                    image_flag = Request(url_flag, headers={'User-Agent': 'Mozilla/5.0'})
+                except urllib.error.HTTPError as e:
+                    print(e.__dict__)
+                    self.btn_download.setEnabled(False)
+                except urllib.error.URLError as e:
+                    print(e.__dict__)
+                    self.btn_download.setEnabled(False)
+                    self.lbl_error.setText('Error!')
+                    self.lbl_error_2.setText(str(e.reason))
+                image_data_flag = urlopen(image_flag).read()
+                self.qPixmapWebVar_flag = QPixmap()
+                self.qPixmapWebVar_flag.loadFromData(image_data_flag)
+                self.qPixmapWebVar_flag = self.qPixmapWebVar_flag.scaledToWidth(120)
+                self.lbl_img_2.setPixmap(self.qPixmapWebVar_flag)
+                self.lbl_img_2.setHidden(False)
+        t = threading.Thread(target=load_img_from_web)
+        t.start()
+        t1 = threading.Thread(target=load_flag_from_web)
+        t1.start()
+    
+        
     
         
     def download(self): 
